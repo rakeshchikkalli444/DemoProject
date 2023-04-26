18b99382b635696122718c0c648f935800e83926
RakeshChikkalli <112151646+RakeshChikkalli@users.noreply.github.com>
2023-04-26 19:53:22 +0530

    Minor Change

---
+++ MVCEX17/MVCEX17/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCEX17.Startup))]
+namespace MVCEX17
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
